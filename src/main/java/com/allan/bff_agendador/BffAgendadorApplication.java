29b3dee74f1a794b511f299fa7e8939414b78a01
Allan <allanfelipedk@gmail.com>
2026-03-05 10:05:56 -0300

    initial commit

---
+++ src/main/java/com/allan/bff_agendador/BffAgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.allan.bff_agendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorApplication.class, args);
+	}
+
+}
